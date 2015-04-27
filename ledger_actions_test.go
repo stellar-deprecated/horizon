e1186f797a0a6e394a861be64a4c01f8e6424106
Scott Fleckenstein <nullstyle@gmail.com>
2015-04-27 15:49:50 -0700

    More work on getting tests stable in travis

--- ledger_actions_test.go
+++ ledger_actions_test.go
@@ -12,45 +12,47 @@ import (
 
 func TestLedgerActions(t *testing.T) {
 
-	Convey("GET /ledgers/1", t, func() {
-		test.LoadScenario("base")
-		app := NewTestApp()
+	Convey("Ledger Actions:", t, func() {
+		Convey("GET /ledgers/1", func() {
+			test.LoadScenario("base")
+			app := NewTestApp()
 
-		r, _ := http.NewRequest("GET", "/ledgers/1", nil)
-		w := httptest.NewRecorder()
-		c := web.C{
-			Env: map[interface{}]interface{}{},
-		}
+			r, _ := http.NewRequest("GET", "/ledgers/1", nil)
+			w := httptest.NewRecorder()
+			c := web.C{
+				Env: map[interface{}]interface{}{},
+			}
 
-		app.web.router.ServeHTTPC(c, w, r)
+			app.web.router.ServeHTTPC(c, w, r)
 
-		var result ledgerResource
-		err := json.Unmarshal(w.Body.Bytes(), &result)
-		So(err, ShouldBeNil)
-		So(w.Code, ShouldEqual, 200)
-		So(result.Sequence, ShouldEqual, 1)
-	})
+			var result ledgerResource
+			err := json.Unmarshal(w.Body.Bytes(), &result)
+			So(err, ShouldBeNil)
+			So(w.Code, ShouldEqual, 200)
+			So(result.Sequence, ShouldEqual, 1)
+		})
 
-	Convey("GET /ledgers", t, func() {
-		test.LoadScenario("base")
-		app := NewTestApp()
+		Convey("GET /ledgers", func() {
+			test.LoadScenario("base")
+			app := NewTestApp()
 
-		r, _ := http.NewRequest("GET", "/ledgers", nil)
-		w := httptest.NewRecorder()
-		c := web.C{
-			Env: map[interface{}]interface{}{},
-		}
+			r, _ := http.NewRequest("GET", "/ledgers", nil)
+			w := httptest.NewRecorder()
+			c := web.C{
+				Env: map[interface{}]interface{}{},
+			}
 
-		app.web.router.ServeHTTPC(c, w, r)
+			app.web.router.ServeHTTPC(c, w, r)
 
-		var result map[string]interface{}
-		err := json.Unmarshal(w.Body.Bytes(), &result)
-		So(err, ShouldBeNil)
-		So(w.Code, ShouldEqual, 200)
+			var result map[string]interface{}
+			err := json.Unmarshal(w.Body.Bytes(), &result)
+			So(err, ShouldBeNil)
+			So(w.Code, ShouldEqual, 200)
 
-		embedded := result["_embedded"].(map[string]interface{})
-		records := embedded["records"].([]interface{})
+			embedded := result["_embedded"].(map[string]interface{})
+			records := embedded["records"].([]interface{})
 
-		So(len(records), ShouldEqual, 4)
+			So(len(records), ShouldEqual, 4)
+		})
 	})
 }
