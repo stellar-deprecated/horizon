6806a36f0bdb497232734806462bf58bbc685b20
Scott Fleckenstein <nullstyle@gmail.com>
2015-05-09 23:46:28 -0700

    Move rate limiter init into init_web.go

--- middleware_rate_limit.go
+++ middleware_rate_limit.go
@@ -1,34 +1,10 @@
 package horizon
 
 import (
-	"github.com/PuerkitoBio/throttled"
-	"github.com/PuerkitoBio/throttled/store"
 	"github.com/zenazn/goji/web"
 	"net/http"
-	"strings"
 )
 
 func (web *Web) RateLimitMiddleware(c *web.C, next http.Handler) http.Handler {
 	return web.rateLimiter.Throttle(next)
-}
-
-func initWebRateLimiter(app *App) {
-	rateLimitStore := store.NewMemStore(1000)
-
-	if app.redis != nil {
-		rateLimitStore = store.NewRedisStore(app.redis, "throttle:", 0)
-	}
-
-	rateLimiter := throttled.RateLimit(
-		app.config.RateLimit,
-		&throttled.VaryBy{Custom: remoteAddrIp},
-		rateLimitStore,
-	)
-	rateLimiter.DeniedHandler = http.HandlerFunc(rateLimitExceededAction)
-	app.web.rateLimiter = rateLimiter
-}
-
-func remoteAddrIp(r *http.Request) string {
-	ip := strings.SplitN(r.RemoteAddr, ":", 2)[0]
-	return ip
 }
