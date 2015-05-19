375621ebf5a89e2f92f9b9b7a74db4e984fbfd2f
Scott Fleckenstein <nullstyle@gmail.com>
2015-05-19 15:37:57 -0700

    Whoops, add back file that was mistakenly removed

---
+++ actions_transaction.go
@@ -0,0 +1,47 @@
+package horizon
+
+import (
+	"net/http"
+
+	"github.com/stellar/go-horizon/db"
+	"github.com/stellar/go-horizon/render"
+	"github.com/stellar/go-horizon/render/problem"
+	"github.com/zenazn/goji/web"
+)
+
+func transactionIndexAction(c web.C, w http.ResponseWriter, r *http.Request) {
+	ah := &ActionHelper{c: c, r: r}
+	app := ah.App()
+
+	q := db.TransactionPageQuery{
+		SqlQuery:       app.HistoryQuery(),
+		PageQuery:      ah.GetPageQuery(),
+		AccountAddress: ah.GetString("account_id"),
+		LedgerSequence: ah.GetInt32("ledger_id"),
+	}
+
+	if ah.Err() != nil {
+		problem.Render(ah.Context(), w, problem.ServerError)
+		return
+	}
+
+	render.Collection(ah.Context(), w, r, q, transactionRecordToResource)
+}
+
+func transactionShowAction(c web.C, w http.ResponseWriter, r *http.Request) {
+	ah := &ActionHelper{c: c, r: r}
+	app := ah.App()
+	hash := ah.GetString("id")
+
+	if ah.Err() != nil {
+		problem.Render(ah.Context(), w, problem.NotFound)
+		return
+	}
+
+	q := db.TransactionByHashQuery{
+		SqlQuery: app.HistoryQuery(),
+		Hash:     hash,
+	}
+
+	render.Single(ah.Context(), w, r, q, transactionRecordToResource)
+}
