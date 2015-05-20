a457ed3f0d7dd15ad9b1ee360e4c9445cc52f6e2
Scott Fleckenstein <nullstyle@gmail.com>
2015-05-20 14:28:27 -0700

    Add additional columns to CoreOfferRecord

--- db/record_core_offer.go
+++ db/record_core_offer.go
@@ -7,10 +7,26 @@ import sq "github.com/lann/squirrel"
 var CoreOfferRecordSelect = sq.Select(
 	"co.accountid",
 	"co.offerid",
+	"co.paysalphanumcurrency",
+	"co.paysissuer",
+	"co.getsalphanumcurrency",
+	"co.getsissuer",
+	"co.amount",
+	"co.pricen",
+	"co.priced",
+	"co.price",
 ).From("offers co")
 
 // CoreOfferRecord is row of data from the `offers` table from stellar-core
 type CoreOfferRecord struct {
-	Accountid string
-	Offerid   int64
+	Accountid            string
+	Offerid              int64
+	Paysalphanumcurrency string
+	Paysissuer           string
+	Getsalphanumcurrency string
+	Getsissuer           string
+	Amount               int64
+	Pricen               int32
+	Priced               int32
+	Price                int64
 }
