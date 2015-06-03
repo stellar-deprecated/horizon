c40661961d171b3b1439f50a13e91ad82473dd22
Scott Fleckenstein <nullstyle@gmail.com>
2015-06-03 09:49:43 -0700

    Add extra fields to loggly

--- log/loggly_hook.go
+++ log/loggly_hook.go
@@ -10,8 +10,9 @@ import (
 
 // LogglyHook sends logs to loggly
 type LogglyHook struct {
-	client *loggly.Client
-	host   string
+	client       *loggly.Client
+	host         string
+	FilteredKeys map[string]bool
 }
 
 // NewLogglyHook creates a new hook
@@ -35,6 +36,15 @@ func (hook *LogglyHook) Fire(entry *logrus.Entry) error {
 		"level":     entry.Level.String(),
 		"message":   entry.Message,
 		"hostname":  hook.host,
+	}
+
+	for k, v := range entry.Data {
+		//Filter out keys
+		if _, ok := hook.FilteredKeys[k]; ok {
+			continue
+		}
+
+		logglyMessage[k] = v
 	}
 
 	return hook.client.Send(logglyMessage)
