6461f7635febdc3392b596f9f2dc1058e9b69dfa
Gregory <gregory.terzian@gmail.com>
2015-06-13 16:43:06 +0800

    formatting

--- db/record_ledger_test.go
+++ db/record_ledger_test.go
@@ -11,12 +11,11 @@ func TestRecord(t *testing.T) {
 
 	Convey("Should be able to set the Id of LegderRecord", t, func() {
 		record := new(LedgerRecord)
-    record.Id = 5
-    So(record.Id, ShouldEqual, 5)
+        record.Id = 5
+        So(record.Id, ShouldEqual, 5)
     
 		Convey("PagingToken() returns an id ", func() {
-      So(record.PagingToken(), ShouldEqual, "5")
+            So(record.PagingToken(), ShouldEqual, "5")
 		})
-
 	})
 }
