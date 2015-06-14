d28a1d116606ce9045bd28ce58d16d71844140bd
Gregory <gregory.terzian@gmail.com>
2015-06-14 15:22:39 +0800

    failing test

--- db/record_operation.go
+++ db/record_operation.go
@@ -14,15 +14,10 @@ var OperationRecordSelect sq.SelectBuilder = sq.
 	From("history_operations hop")
 
 type OperationRecord struct {
-	Id               int64             `db:"id"`
 	TransactionId    int64             `db:"transaction_id"`
 	ApplicationOrder int32             `db:"application_order"`
 	Type             xdr.OperationType `db:"type"`
 	DetailsString    sql.NullString    `db:"details"`
-}
-
-func (r OperationRecord) PagingToken() string {
-	return fmt.Sprintf("%d", r.Id)
 }
 
 func (r OperationRecord) Details() (result map[string]interface{}, err error) {
