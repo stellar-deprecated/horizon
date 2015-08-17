7f5e7c392af17565f9ef39b74469fc2d4eaf242b
Scott Fleckenstein <nullstyle@gmail.com>
2015-08-17 16:13:53 -0700

    Remove debug line

--- src/github.com/stellar/go-horizon/actions_offer_test.go
+++ src/github.com/stellar/go-horizon/actions_offer_test.go
@@ -18,8 +18,6 @@ func TestOfferActions(t *testing.T) {
 		Convey("GET /accounts/GAJFK65MU3WQW4PZYJXBS7LXLXHHZB2RNVX7EC6DUZYU2NE4VMANPX2W/offers", func() {
 			w := rh.Get("/accounts/GAJFK65MU3WQW4PZYJXBS7LXLXHHZB2RNVX7EC6DUZYU2NE4VMANPX2W/offers", test.RequestHelperNoop)
 
-			t.Log(w.Body.String())
-
 			So(w.Code, ShouldEqual, 200)
 			So(w.Body, ShouldBePageOf, 3)
 		})
