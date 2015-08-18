c4e96188444300566b21417b46e7237b4aaab3e9
Frederic Heem <frederic.heem@finclusionsystems.com>
2015-08-18 09:39:24 +0100

    set Access-Control-Allow-Origin to *

--- src/github.com/stellar/go-horizon/middleware_app.go
+++ src/github.com/stellar/go-horizon/middleware_app.go
@@ -10,6 +10,9 @@ import (
 func (app *App) Middleware(c *web.C, h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		c.Env["app"] = app
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Headers","Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
 		h.ServeHTTP(w, r)
 	})
 }
