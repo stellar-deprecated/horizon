854ef6c2e241d628c6d221b6b015bcfcbe77f3bd
Scott Fleckenstein <nullstyle@gmail.com>
2015-08-24 08:44:35 -0700

    Correct root resource link to real order_book route

--- src/github.com/stellar/go-horizon/actions_root.go
+++ src/github.com/stellar/go-horizon/actions_root.go
@@ -19,7 +19,7 @@ var globalRootResource = RootResource{
 		Link("account_transactions", "/accounts/{address}/transactions{?cursor,limit,order}").
 		Link("transaction", "/transactions/{hash}").
 		Link("transactions", "/transactions{?cursor,limit,order}").
-		Link("orderbook", "/orderbooks{?base_type,base_code,base_issuer,counter_type,counter_code,counter_issuer}").
+		Link("order_book", "/order_book{?selling_type,selling_code,selling_issuer,buying_type,buying_code,buying_issuer}").
 		Link("metrics", "/metrics").
 		Link("friendbot", "/friendbot{?addr}"),
 }
