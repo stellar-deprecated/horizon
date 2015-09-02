bb02405359f0a728669f516f5a758bb94a35529b
Bartek Nowotarski <bartek@nowotarski.info>
2015-09-02 16:28:56 +0200

    Added created_at field to transaction resource #76

--- src/github.com/stellar/go-horizon/db/record_transaction.go
+++ src/github.com/stellar/go-horizon/db/record_transaction.go
@@ -8,13 +8,15 @@ import (
 
 // Provides a squirrel.SelectBuilder upon which you may build actual queries.
 var TransactionRecordSelect sq.SelectBuilder = sq.
-	Select("ht.*").
-	From("history_transactions ht")
+	Select("ht.*, hl.closed_at AS ledger_close_time").
+	From("history_transactions ht").
+	LeftJoin("history_ledgers hl ON ht.ledger_sequence = hl.sequence")
 
 type TransactionRecord struct {
 	HistoryRecord
 	TransactionHash     string         `db:"transaction_hash"`
 	LedgerSequence      int32          `db:"ledger_sequence"`
+	LedgerCloseTime     time.Time      `db:"ledger_close_time"`
 	ApplicationOrder    int32          `db:"application_order"`
 	Account             string         `db:"account"`
 	AccountSequence     int64          `db:"account_sequence"`
