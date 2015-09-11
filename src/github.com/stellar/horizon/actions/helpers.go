08ab1b3d91b9c08467ee9b87a4a96223e5db7c0c
Scott Fleckenstein <nullstyle@gmail.com>
2015-09-11 15:33:57 -0700

    Extract `actions.GetOrderBook`

--- src/github.com/stellar/horizon/actions/helpers.go
+++ src/github.com/stellar/horizon/actions/helpers.go
@@ -1,11 +1,13 @@
 package actions
 
 import (
+	"net/http"
 	"strconv"
 
+	"github.com/stellar/go-stellar-base/xdr"
 	"github.com/stellar/horizon/assets"
 	"github.com/stellar/horizon/db"
-	"github.com/stellar/go-stellar-base/xdr"
+	"github.com/stellar/horizon/render/problem"
 )
 
 const (
@@ -16,6 +18,17 @@ const (
 	// ParamLimit is a query string param name
 	ParamLimit = "limit"
 )
+
+// OrderBookParams is a helper struct that encapsulates the specification for
+// an order book
+type OrderBookParams struct {
+	SellingType   xdr.AssetType
+	SellingIssuer string
+	SellingCode   string
+	BuyingType    xdr.AssetType
+	BuyingIssuer  string
+	BuyingCode    string
+}
 
 // GetString retrieves a string from either the URLParams, form or query string.
 // This method uses the priority (URLParams, Form, Query).
@@ -141,6 +154,62 @@ func (base *Base) GetAssetType(name string) xdr.AssetType {
 	}
 
 	return r
+}
+
+// GetOrderBook returns an OrderBookParams from the url params
+func (base *Base) GetOrderBook() (result OrderBookParams) {
+	if base.Err != nil {
+		return
+	}
+
+	result = OrderBookParams{
+		SellingType:   base.GetAssetType("selling_asset_type"),
+		SellingIssuer: base.GetString("selling_asset_issuer"),
+		SellingCode:   base.GetString("selling_asset_code"),
+		BuyingType:    base.GetAssetType("buying_asset_type"),
+		BuyingIssuer:  base.GetString("buying_asset_issuer"),
+		BuyingCode:    base.GetString("buying_asset_code"),
+	}
+
+	if base.Err != nil {
+		goto InvalidOrderBook
+	}
+
+	if result.SellingType != xdr.AssetTypeAssetTypeNative {
+		if result.SellingCode == "" {
+			goto InvalidOrderBook
+		}
+
+		if result.SellingIssuer == "" {
+			goto InvalidOrderBook
+		}
+	}
+
+	if result.BuyingType != xdr.AssetTypeAssetTypeNative {
+		if result.BuyingCode == "" {
+			goto InvalidOrderBook
+		}
+
+		if result.BuyingIssuer == "" {
+			goto InvalidOrderBook
+		}
+	}
+
+	return
+
+InvalidOrderBook:
+	base.Err = &problem.P{
+		Type:   "invalid_order_book",
+		Title:  "Invalid Order Book Parameters",
+		Status: http.StatusBadRequest,
+		Detail: "The parameters that specify what order book to view are invalid in some way. " +
+			"Please ensure that your type parameters (selling_asset_type and buying_asset_type) are one the " +
+			"following valid values: native, credit_alphanum4, credit_alphanum12.  Also ensure that you " +
+			"have specified selling_asset_code and selling_issuer if selling_asset_type is not 'native', as well " +
+			"as buying_asset_code and buying_issuer if buying_asset_type is not 'native'",
+	}
+
+	return
 }
 
 // Path returns the current action's path, as determined by the http.Request of
