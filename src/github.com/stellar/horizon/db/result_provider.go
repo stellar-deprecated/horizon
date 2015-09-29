b3ccb10f3fbe035c6a1525d0d31736b6c86c456e
Scott Fleckenstein <nullstyle@gmail.com>
2015-09-29 09:04:24 -0700

    Implement stellar-core check for db.ResultProvider

--- src/github.com/stellar/horizon/db/result_provider.go
+++ src/github.com/stellar/horizon/db/result_provider.go
@@ -12,10 +12,9 @@ type ResultProvider struct {
 }
 
 func (rp *ResultProvider) ResultByHash(ctx context.Context, hash string) txsub.Result {
-	var (
-		hr TransactionRecord
-	)
 
+	// query history database
+	var hr TransactionRecord
 	hq := TransactionByHashQuery{
 		SqlQuery: SqlQuery{rp.History},
 		Hash:     hash,
@@ -23,14 +22,28 @@ func (rp *ResultProvider) ResultByHash(ctx context.Context, hash string) txsub.R
 
 	err := Get(ctx, hq, &hr)
 	if err == nil {
-		return txResultFromTransactionRecord(hr)
+		return txResultFromHistory(hr)
 	}
 
 	if err != ErrNoResults {
 		return txsub.Result{Err: err}
 	}
 
-	//TODO: check stellar-core for results as well
+	// query core database
+	var cr CoreTransactionRecord
+	cq := CoreTransactionByHashQuery{
+		SqlQuery: SqlQuery{rp.Core},
+		Hash:     hash,
+	}
+
+	err = Get(ctx, cq, &cr)
+	if err == nil {
+		return txResultFromCore(cr)
+	}
+
+	if err != ErrNoResults {
+		return txsub.Result{Err: err}
+	}
 
 	return txsub.Result{Err: txsub.ErrNoResults}
 }
@@ -46,7 +59,7 @@ func (rp *ResultProvider) ResultByAddressAndSequence(ctx context.Context, addr s
 
 	err := Get(ctx, hq, &hr)
 	if err == nil {
-		return txResultFromTransactionRecord(hr)
+		return txResultFromHistory(hr)
 	}
 
 	if err != ErrNoResults {
@@ -58,12 +71,22 @@ func (rp *ResultProvider) ResultByAddressAndSequence(ctx context.Context, addr s
 	return txsub.Result{Err: txsub.ErrNoResults}
 }
 
-func txResultFromTransactionRecord(hr TransactionRecord) txsub.Result {
+func txResultFromHistory(tx TransactionRecord) txsub.Result {
 	return txsub.Result{
-		Hash:           hr.TransactionHash,
-		LedgerSequence: hr.LedgerSequence,
-		EnvelopeXDR:    hr.TxEnvelope.String,
-		ResultXDR:      hr.TxResult.String,
-		ResultMetaXDR:  hr.TxMeta.String,
+		Hash:           tx.TransactionHash,
+		LedgerSequence: tx.LedgerSequence,
+		EnvelopeXDR:    tx.TxEnvelope.String,
+		ResultXDR:      tx.TxResult.String,
+		ResultMetaXDR:  tx.TxMeta.String,
+	}
+}
+
+func txResultFromCore(tx CoreTransactionRecord) txsub.Result {
+	return txsub.Result{
+		Hash:           tx.TransactionHash,
+		LedgerSequence: tx.LedgerSequence,
+		EnvelopeXDR:    tx.EnvelopeXDR,
+		ResultXDR:      tx.ResultXDR,
+		ResultMetaXDR:  tx.ResultMetaXDR,
 	}
 }
