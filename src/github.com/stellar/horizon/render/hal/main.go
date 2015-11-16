3dcbfe5e5fca69d02e3e91c8f7f763e9e088b4bb
Scott Fleckenstein <nullstyle@gmail.com>
2015-11-16 14:25:27 -0800

    Remove unused dependencies

--- src/github.com/stellar/horizon/render/hal/main.go
+++ src/github.com/stellar/horizon/render/hal/main.go
@@ -3,18 +3,11 @@ package hal
 import (
 	"encoding/json"
 	"net/http"
-
-	"github.com/jagregory/halgo"
 )
 
 // StandardPagingOptions is a helper string to make creating paged collection
 // URIs simpler.
 var StandardPagingOptions = "{?cursor,limit,order}"
-
-type Page struct {
-	halgo.Links
-	Records []interface{}
-}
 
 type Pageable interface {
 	PagingToken() string
@@ -31,15 +24,6 @@ func RenderToString(data interface{}, pretty bool) ([]byte, error) {
 
 // Render write data to w, after marshalling to json
 func Render(w http.ResponseWriter, data interface{}) {
-	if page, ok := data.(Page); ok {
-		data = map[string]interface{}{
-			"_links": page.Items,
-			"_embedded": map[string]interface{}{
-				"records": page.Records,
-			},
-		}
-	}
-
 	js, err := RenderToString(data, true)
 
 	if err != nil {
