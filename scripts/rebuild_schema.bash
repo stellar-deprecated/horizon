1626bbe05e844d20957397962c2ff9f2e1fa2674
Scott Fleckenstein <nullstyle@gmail.com>
2016-03-28 15:20:42 -0700

    Index participant tables to improve delete performance and scalability

--- scripts/rebuild_schema.bash
+++ scripts/rebuild_schema.bash
@@ -3,15 +3,15 @@ set -e
 
 # This scripts rebuilds the latest.sql file included in the schema package.
 
-gb generate github.com/stellar/horizon/db/schema
+gb generate github.com/stellar/horizon/db2/schema
 gb build
 dropdb horizon_schema --if-exists
 createdb horizon_schema
 DATABASE_URL=postgres://localhost/horizon_schema?sslmode=disable ./bin/horizon db migrate up
 
-pg_dump postgres://localhost/horizon_schema?sslmode=disable --clean --if-exists --no-owner --no-acl --inserts > src/github.com/stellar/horizon/db/schema/latest.sql
-cp src/github.com/stellar/horizon/db/schema/latest.sql src/github.com/stellar/horizon/test/scenarios/blank-horizon.sql
+pg_dump postgres://localhost/horizon_schema?sslmode=disable --clean --if-exists --no-owner --no-acl --inserts > src/github.com/stellar/horizon/db2/schema/latest.sql
+cp src/github.com/stellar/horizon/db2/schema/latest.sql src/github.com/stellar/horizon/test/scenarios/blank-horizon.sql
 
-gb generate github.com/stellar/horizon/db/schema
+gb generate github.com/stellar/horizon/db2/schema
 gb generate github.com/stellar/horizon/test
 gb build
