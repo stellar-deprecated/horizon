327daf19891da22dcc92ef05bd371133606d3d5a
Scott Fleckenstein <nullstyle@gmail.com>
2016-06-29 09:31:19 -0700

    Removes unnecessary find clause from test scenario script

--- scripts/build_test_scenarios.bash
+++ scripts/build_test_scenarios.bash
@@ -3,7 +3,7 @@ set -e
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 PACKAGES=$(find src/github.com/stellar/horizon/test/scenarios -iname '*.rb' -not -name '_common_accounts.rb')
-# PACKAGES=$(find src/github.com/stellar/horizon/test/scenarios -iname 'kahuna.rb' -not -name '_common_accounts.rb')
+# PACKAGES=$(find src/github.com/stellar/horizon/test/scenarios -iname 'kahuna.rb')
 
 gb build
 
