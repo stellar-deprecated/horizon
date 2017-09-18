5f0c57189dbb152b03d8f3df184736f6eb67cfe4
Alex Peters <alpe@users.noreply.github.com>
2017-09-18 10:30:29 +0200

    Run Go test sequential as before

--- scripts/run_tests.bash
+++ scripts/run_tests.bash
@@ -2,4 +2,16 @@
 
 set -e
 
-go test  $(go list ./... | grep -v "/vendor/")
+PACKAGES=$(go list ./... | grep -v "/vendor/")
+
+for i in $PACKAGES; do
+	has_tests=`ls -1 $GOPATH/src/$i/*_test.go 2>/dev/null | wc -l`
+
+	if [ $has_tests != 0 ]; then
+		go test ${i}
+	else
+		echo "skipping $i, no tests"
+	fi
+done
+
+echo "All tests pass!"
